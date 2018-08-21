875b7d4ea6a019ec3e2d8e181ef4b02c34d3800c
Alexandre Gomes <alegomes@gmail.com>
2018-08-21 11:20:48 -0300

    better logging

--- testurls.py
+++ testurls.py
@@ -12,14 +12,14 @@ for files in glob.glob("input/urls-*.csv"):
 		for row in f:
 			url = row.strip()
 			print('\n-------------------------------------------------------')
-			print('Reading url %s\n' % url)
+			print('[%s] Reading url %s\n' % (file,url))
 			p1 = Popen(["./casperjs", "testurl.js", url])
 			output = p1.communicate()[0]
 			status = p1.returncode
 
 			print('Status code',status)
 			if status != 0:
-				print('CasperJS Failed')
+				print('[%s,%s] CasperJS Failed' % (file,url))
 				err = open('urlscomproblema.txt', 'a')
 				err.write(url+"\n")
 				err.close()
